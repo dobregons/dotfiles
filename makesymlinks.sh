06b7184f8576796515464717e963c7856edccb79
David Obregon <david.obregon@contractor.jupiterone.com>
2023-07-26 08:51:45 -0700

    fix errors

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc .p10k.zsh."    # list of files/folders to symlink in homedir
+files="zshrc .p10k.zsh. gitconfig saml2aws vimrc vim aws"    # list of files/folders to symlink in homedir
 
 ##########
 
